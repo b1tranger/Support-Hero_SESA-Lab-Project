e3a039e8307caa97e8eab4305ec42bef3ab765b8
BitRanger <63799660+b1tranger@users.noreply.github.com>
2025-11-12 05:39:22 +0600

    Update connection.php

--- Gaus_Files/connection.php
+++ Gaus_Files/connection.php
@@ -8,3 +8,15 @@ $conn = new mysqli($name, $username, $password, $db_name);
 
 
 ?>
+
+
+<?php
+
+// $name = "sql212.infinityfree.com";
+// $username = "if0_40351225";
+// $password = "grp7sesaproject";
+// $db_name = "if0_40351225_project_db";
+// $conn = new mysqli($name, $username, $password, $db_name);
+
+
+?>
